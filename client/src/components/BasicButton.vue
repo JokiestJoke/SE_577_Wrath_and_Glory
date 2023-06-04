a60ae2a0670713aefc3d61d8530e02c0f3ab6abd
Mark <mdoghram0@gmail.com>
2023-06-03 20:43:29 -0400

    fixed major bug with the Vue button component, where half the message would render rather than the entirety.

---
+++ client/src/components/BasicButton.vue
@@ -0,0 +1,22 @@
+<template>
+
+  <button @click="onClick" class="BasicButton">
+    {{ message }}
+  </button>
+
+</template>
+
+<script lang="ts">
+export default {
+  name: 'BasicButton',
+  props: {
+    message: {
+      type: String
+    },
+    onClick: {
+      type: Function,
+      required: true
+    },
+  },
+};
+</script>
--- client/src/pages/CharacterCreation.vue
+++ client/src/pages/CharacterCreation.vue
@@ -4,6 +4,11 @@
       <p> This is the character creation page. Here you can choose
         between all "Tier One" Wrath and Glory Archetypes!</p>
     </div>
+    <div> <br>
+      <p>
+        Archetype List:
+      </p>
+    </div>
   </div>
 
   <div v-if="tierOneArchetypeData.length > 0">
@@ -28,23 +33,23 @@
           <td>{{ archetype.attributes }}</td>
           <td>{{ archetype.skills }}</td>
           <td>{{ archetype.ability }}</td>
-
-          <SelectArchetypeButton :character="archetype.archetypeTitle" :onClick="archetypeSelectClick"></SelectArchetypeButton>
-
+          <!-- <SelectArchetypeButton :character="archetype.archetypeTitle" :onClick="archetypeSelectClick"></SelectArchetypeButton> -->
         </tr>
-
       </table>
+      <div>
+        <BasicButton :message="createCharacterButtonMessage" :onClick="createCharacter"></BasicButton>
+      </div>
     </div>
   </div>
 
   <div id="CharacterSheet">
     <table id="CharacterInformationTable">
       <tr>
-        <th id="CharacterInformation">Character Name</th>
-        <th id="CharacterInformation">Species</th>
-        <th id="CharacterInformation">Archetype Title</th>
-        <th id="CharacterInformation">Faction</th>
-        <th id="CharacterInformation">Keywords</th>
+        <th>Character Name</th>
+        <th>Species</th>
+        <th>Archetype Title</th>
+        <th>Faction</th>
+        <th>Keywords</th>
       </tr>
 
       <tr>
@@ -57,8 +62,9 @@
         <td>Test</td>
         <td>Test</td>
       </tr>
-
     </table>
+
+
   </div>
 
 
@@ -67,12 +73,11 @@
 <script lang="ts">
 export default {
   name: 'CharacterCreationPage',
-  component: SelectArchetypeButton,
+  component: BasicButton,
   methods: {
-    archetypeSelectClick(){
+    createCharacter(){
       document.getElementById("CharacterSheet").style.visibility = "visible";
       //chosenArchetypeData =
-
     }
   }
 };
@@ -82,20 +87,18 @@ export default {
 import { onMounted, ref } from 'vue';
 import type {TierOneArchetypes} from './ApiInterfaces';
 import SelectArchetypeButton from '../components/SelectArchetypeButton.vue';
+import BasicButton from '../components/BasicButton.vue'
 import axios from 'axios';
 
 //Most code goes here
 let tierOneArchetypeData = ref<TierOneArchetypes[]>([])
-let chosenArchetype = null;
 
-let buttonTitle = ' Button';
-let inquisitorTitle = 'Inquisitor ';
+let createCharacterButtonMessage = "Create a Character";
 
 onMounted(async () => {
   console.log("Character Creation mounted")
 
   let tierOneArchetypeURI = 'http://localhost:9500/archetypes'
-
 
   //Use axios to load the student data - readup on await to make
   //async calls easier
